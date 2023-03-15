acc2f5338fd9f5ba13ad0c3283559ae4c5809ba8
VarunSahasrabudhe <varun.sahasrabudhe@arrow.com>
2023-03-15 12:13:58 -0700

    Data Ingestion pipeline

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+from src.logger import logging
+from src.exception import CustomException
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artficats','train.csv')
+    test_data_path: str = os.path.join('artficats','test.csv')
+    raw_data_path: str = os.path.join('artficats','raw.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+    
+    ## Function to read the data from database or a system file
+    def initate_data_ingestion(self):
+        logging.info("Enter Data Ingestion method")
+        try:
+            df = pd.read_csv('data\stud.csv') ## Can change this code to initiate a db instance
+            logging.info("Successfully read the data from the data file into a dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split initiated")
+            train_data,test_data = train_test_split(df,test_size=0.2,random_state = 42)
+
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+
+            logging.info("Ingestion of the data is completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+    
--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logger
+from src import logger
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     filename = exc_tb.tb_frame.f_code.co_filename
